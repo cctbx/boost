df61018f6d292152a91f0e485fef36d4ec2440eb
Billy K. Poon <bkpoon@lbl.gov>
2024-09-11 12:44:17 -0700

    Remove C++20 path in Boost.filesystem

--- libs/filesystem/src/atomic_ref.hpp
+++ libs/filesystem/src/atomic_ref.hpp
@@ -13,7 +13,7 @@
 #define BOOST_FILESYSTEM_SRC_ATOMIC_REF_HPP_
 
 #include <boost/filesystem/config.hpp>
-
+/*
 #if !defined(BOOST_FILESYSTEM_NO_CXX20_ATOMIC_REF)
 
 #include <atomic>
@@ -21,12 +21,12 @@
 namespace atomic_ns = std;
 
 #else // !defined(BOOST_FILESYSTEM_NO_CXX20_ATOMIC_REF)
-
+*/
 #include <boost/memory_order.hpp>
 #include <boost/atomic/atomic_ref.hpp>
 
 namespace atomic_ns = boost;
-
+/*
 #endif // !defined(BOOST_FILESYSTEM_NO_CXX20_ATOMIC_REF)
-
+*/
 #endif // BOOST_FILESYSTEM_SRC_ATOMIC_REF_HPP_
